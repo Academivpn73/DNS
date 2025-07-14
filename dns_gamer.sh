beafca886d70ef9d688f1b8457a49bf4e1d2108a
Academivpn73 <academi.mah43@gmail.com>
2025-07-15 01:44:26 +0330

    Update dns_gamer.sh

--- dns_gamer.sh
+++ dns_gamer.sh
@@ -1,84 +1,136 @@
 #!/bin/bash
 clear
-GREEN='\e[92m'; RESET='\e[0m'
+GREEN='\e[92m'; RED='\e[91m'; YELLOW='\e[93m'; RESET='\e[0m'
 
-echo -e "${GREEN}🎮 DNS Gaming Tool v2.2"
+echo -e "${GREEN}🎮 DNS Gaming Tool v3.0"
 echo "Telegram: @Academi_vpn  |  Admin: @MahdiAGM0"
 echo -e "═════════════════════════════════════════════${RESET}"
 
-# انتخاب نوع DNS
-echo -e "\nChoose DNS Type:"
+# 1. انتخاب بازی
+games=(
+"PUBG Mobile" "Fortnite" "Call of Duty: Mobile" "Free Fire"
+"Valorant" "Apex Legends" "CS2" "League of Legends" "Dota 2" "FIFA"
+"Overwatch 2" "Minecraft" "Mobile Legends" "Wild Rift" "Clash Royale"
+"Clash of Clans" "Among Us" "Warzone" "Genshin Impact" "Brawl Stars"
+"Rocket League" "Honkai: Star Rail" "Team Fortress 2" "Rust" "Roblox"
+"Battlefield" "Rainbow Six Siege" "ARK: Survival" "Smite" "Paladins"
+)
+
+echo -e "${YELLOW}\nSelect the game you play:${RESET}"
+select game in "${games[@]}"; do
+  [[ -n "$game" ]] && break
+done
+
+# 2. انتخاب IPv4 یا IPv6
+echo -e "${YELLOW}\nChoose DNS IP Version:${RESET}"
 echo "1) IPv4"
 echo "2) IPv6"
-read -p "Select (1 or 2): " iptype
+read -p "Select (1 or 2): " ipver
 
-# انتخاب کشور (نمایشی، تاثیری در DNSها نداره)
-countries=(Iran Germany USA Japan Singapore Netherlands France Turkey UK Canada)
-echo -e "\nSelect Country:"
+# 3. انتخاب کشور
+countries=("Iran" "Germany" "Netherlands" "Singapore" "USA" "France" "Turkey" "India" "UK" "Japan")
+echo -e "${YELLOW}\nSelect Region:${RESET}"
 select country in "${countries[@]}"; do
   [[ -n "$country" ]] && break
 done
 
-# لیست DNSها (برای هر دو حالت)
-dns_list_ipv4=(
-"1.1.1.1|Cloudflare"
-"8.8.8.8|Google"
-"9.9.9.9|Quad9"
-"45.90.28.0|NextDNS"
-"208.67.222.222|OpenDNS"
-"94.140.14.14|AdGuard"
-"185.228.168.9|CleanBrowsing"
-"77.88.8.8|Yandex"
-"76.76.2.0|ControlD"
-"89.233.43.71|UncensoredDNS"
-)
+# 4. لیست DNS ها برای هر کشور
+get_dns_list() {
+  local iptype="$1"
+  local region="$2"
 
-dns_list_ipv6=(
-"2606:4700:4700::1111|Cloudflare"
-"2001:4860:4860::8888|Google"
-"2620:fe::fe|Quad9"
-"2a0d:2a00:1::1|NextDNS"
-"2620:119:35::35|OpenDNS"
-"2a10:50c0::ad1:ff|AdGuard"
-"2a00:5a60::ad1:0ff|CleanBrowsing"
-"2a00:5a60::ad2:0ff|Yandex"
-"2606:4700:4700::1001|ControlD"
-"2a10:50c0::ad2:ff|UncensoredDNS"
-)
+  if [[ "$iptype" == "4" ]]; then
+    case "$region" in
+      "Iran") dns=(
+        "178.22.122.100|Shecan"
+        "185.51.200.2|Electro"
+        "10.202.10.10|Local"
+        "185.105.239.2|Shatel"
+        "185.55.225.25|Radar"
+        "10.202.10.202|Private"
+        "185.231.182.1|Respina"
+        "185.51.200.3|HyperNET"
+        "185.208.173.1|AsiaTech"
+        "178.22.122.101|Shecan 2"
+      ) ;;
+      "Germany") dns=(
+        "45.90.28.0|NextDNS"
+        "9.9.9.11|Quad9"
+        "1.1.1.1|Cloudflare"
+        "8.8.8.8|Google"
+        "94.140.14.14|AdGuard"
+        "94.130.180.225|Level3"
+        "176.103.130.130|AdGuard"
+        "80.241.218.68|Uncensored"
+        "84.200.69.80|DNS.WATCH"
+        "208.67.222.222|OpenDNS"
+      ) ;;
+      "USA") dns=(
+        "8.8.8.8|Google"
+        "1.0.0.1|Cloudflare"
+        "9.9.9.9|Quad9"
+        "208.67.222.222|OpenDNS"
+        "4.2.2.2|Level3"
+        "205.171.3.65|Sprint"
+        "8.26.56.26|Comodo"
+        "156.154.70.1|Neustar"
+        "4.2.2.4|Verizon"
+        "129.250.35.250|NTT"
+      ) ;;
+      # سایر کشورها (با DNS معروف و با پینگ مناسب)
+      *) dns=(
+        "1.1.1.1|Cloudflare"
+        "8.8.8.8|Google"
+        "9.9.9.9|Quad9"
+        "208.67.222.222|OpenDNS"
+        "94.140.14.14|AdGuard"
+        "45.90.28.0|NextDNS"
+        "76.76.2.0|ControlD"
+        "185.228.168.9|CleanBrowsing"
+        "77.88.8.8|Yandex"
+        "89.233.43.71|UncensoredDNS"
+      ) ;;
+    esac
+  else
+    dns=(
+      "2606:4700:4700::1111|Cloudflare"
+      "2001:4860:4860::8888|Google"
+      "2a0d:2a00:1::1|NextDNS"
+      "2620:fe::fe|Quad9"
+      "2a10:50c0::ad1:ff|AdGuard"
+      "2620:119:35::35|OpenDNS"
+      "2a00:5a60::ad2:0ff|CleanBrowsing"
+      "2a00:5a60::ad1:0ff|ControlD"
+      "2a00:5a60::1|Yandex"
+      "2a10:50c0::ad2:ff|UncensoredDNS"
+    )
+  fi
 
-echo -e "\n${GREEN}Testing DNS servers (IPv$iptype)...${RESET}"
+  # خروجی پینگ DNS
+  echo -e "\n${GREEN}Top 10 Optimized DNS for $game - $region (IPv$iptype):${RESET}"
+  i=1
+  for entry in "${dns[@]}"; do
+    ip="${entry%%|*}"
+    name="${entry##*|}"
 
-i=1
-count=0
-
-if [[ "$iptype" == "1" ]]; then
-  for dns_entry in "${dns_list_ipv4[@]}"; do
-    ip="${dns_entry%%|*}"
-    name="${dns_entry##*|}"
-    ping_result=$(ping -c1 -W1 "$ip" 2>/dev/null | grep 'time=' | sed -E 's/.*time=([0-9.]+).*/\1/')
-    if [[ "$ping_result" != "" ]]; then
-      printf "%2d) %-15s [%-12s]  🟢 %sms\n" "$i" "$ip" "$name" "$ping_result"
-      ((i++))
-      ((count++))
+    if [[ "$ipver" == "1" ]]; then
+      ping_val=$(ping -c1 -W1 "$ip" 2>/dev/null | grep time= | sed -E 's/.*time=([0-9.]+).*/\1/')
+    else
+      ping_val=$(ping6 -c1 -W1 "$ip" 2>/dev/null | grep time= | sed -E 's/.*time=([0-9.]+).*/\1/')
     fi
-    [[ $count -ge 10 ]] && break
-  done
-else
-  for dns_entry in "${dns_list_ipv6[@]}"; do
-    ip="${dns_entry%%|*}"
-    name="${dns_entry##*|}"
-    ping_result=$(ping6 -c1 -W1 "$ip" 2>/dev/null | grep 'time=' | sed -E 's/.*time=([0-9.]+).*/\1/')
-    if [[ "$ping_result" != "" ]]; then
-      printf "%2d) %-38s [%-12s]  🟢 %sms\n" "$i" "$ip" "$name" "$ping_result"
+
+    if [[ "$ping_val" != "" ]]; then
+      printf "%2d) %-40s [%-15s] 🟢 %s ms\n" "$i" "$ip" "$name" "$ping_val"
       ((i++))
-      ((count++))
     fi
-    [[ $count -ge 10 ]] && break
+    [[ $i -gt 10 ]] && break
   done
-fi
+}
 
-if [[ $count -eq 0 ]]; then
-  echo -e "${RED}❌ No DNS responded. Try again or check your internet.${RESET}"
-fi
+# تعیین IP ورژن
+[[ "$ipver" == "1" ]] && iptype="4" || iptype="6"
 
-echo -e "\n${GREEN}✅ Done. Apply these DNS to your system/game manually.${RESET}"
+# فراخوانی تابع نمایش DNS
+get_dns_list "$iptype" "$country"
+
+echo -e "\n${YELLOW}✅ Copy your favorite DNS and set it in your system/router manually.${RESET}"
