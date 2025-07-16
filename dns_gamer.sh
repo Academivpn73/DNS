9fc63026aeb680be2da8c21a62d3853087bd0644
Academivpn73 <academi.mah43@gmail.com>
2025-07-16 17:12:55 +0330

    Update dns_gamer.sh

--- dns_gamer.sh
+++ dns_gamer.sh
@@ -1,172 +1,93 @@
 #!/bin/bash
 
 # Auto-install required packages
-for cmd in figlet lolcat; do
-    if ! command -v $cmd &>/dev/null; then
-        echo "Installing missing package: $cmd"
-        apt update -y && apt install -y $cmd
+install_deps() {
+  for pkg in figlet lolcat iputils-ping; do
+    if ! command -v "$pkg" &> /dev/null; then
+      echo -e "Installing $pkg..." | lolcat
+      apt install "$pkg" -y &> /dev/null
     fi
-done
-
-# Random color function
-colors=(31 32 33 34 36 91 92 93 94 96)
-rand_color() {
-    echo -e "\e[1;${colors[$RANDOM % ${#colors[@]}]}m$1\e[0m"
+  done
 }
 
-# Title
+install_deps
+
 clear
-figlet -c "MAHDI DNS" | lolcat
-echo -e "$(rand_color 'Telegram:') @Academi_vpn"
-echo -e "$(rand_color 'Admin:') @MahdiAGM0"
-echo -e "\e[1;31mVersion: 1.2.0\e[0m\n"
+# Header
+figlet "DNS Tool" | lolcat
+echo -e "\e[93mTelegram:\e[0m @Academi_vpn"
+echo -e "\e[93mAdmin:\e[0m @MahdiAGM0"
+echo -e "\e[91mVersion:\e[0m 1.2.0"
+echo ""
 
 # Game List
 games=(
-"PUBG Mobile"
-"Call of Duty Mobile"
-"Fortnite"
-"Valorant"
-"Free Fire"
-"Clash of Clans"
-"Clash Royale"
-"Apex Legends"
-"League of Legends"
-"Dota 2"
-"CS:GO"
-"Overwatch"
-"Rocket League"
-"FIFA Mobile"
-"Mobile Legends"
-"Brawl Stars"
-"Roblox"
-"Among Us"
-"Warzone"
-"Battlefield"
-"Rainbow Six Siege"
-"World of Tanks"
-"ARK Survival"
-"Paladins"
-"Smite"
-"Genshin Impact"
-"Diablo Immortal"
-"NBA 2K"
-"eFootball"
-"Star Wars Battlefront"
-"Minecraft"
-"Rust"
-"Escape From Tarkov"
-"DayZ"
-"Dead by Daylight"
-"Hearthstone"
-"EVE Online"
-"Pokemon Unite"
-"Naruto Blazing"
-"Injustice 2"
+  "Call of Duty" "PUBG" "Free Fire" "Fortnite" "Apex Legends" "Valorant"
+  "League of Legends" "Dota 2" "Overwatch" "CS:GO" "Rainbow Six Siege"
+  "Minecraft" "Warzone" "Genshin Impact" "Brawl Stars" "Mobile Legends"
+  "Arena of Valor" "FIFA Online" "NBA 2K" "Rocket League" "Smite"
+  "World of Warcraft" "Elden Ring" "Tarkov" "Lost Ark"
+  "Battlefield" "Cyberpunk" "Paladins" "Clash Royale" "Roblox"
+  "Honkai: Star Rail" "Black Desert" "Destiny 2" "The Finals"
+  "Diablo IV" "Overcooked 2" "ARK" "Rust" "The Crew"
 )
 
-# Country DNS map (gaming)
+# Country List (Middle East)
+countries=("Iran" "Turkey" "UAE" "Saudi Arabia" "Iraq" "Qatar" "Jordan" "Oman" "Kuwait" "Bahrain")
+
+# DNS Mapping
 declare -A dns_map
-dns_map["Iran"]="178.22.122.100 185.51.200.2"
-dns_map["Turkey"]="185.33.33.33 185.38.38.38"
-dns_map["UAE"]="91.74.107.180 91.74.107.181"
-dns_map["Saudi Arabia"]="195.229.241.222 195.229.240.42"
-dns_map["Iraq"]="185.73.112.53 185.73.112.54"
-dns_map["Qatar"]="89.211.30.2 89.211.30.3"
-dns_map["Egypt"]="41.128.225.1 41.128.225.2"
-dns_map["Jordan"]="212.35.65.1 212.35.65.2"
-dns_map["Oman"]="82.178.72.30 82.178.72.31"
-dns_map["Kuwait"]="62.150.97.200 62.150.97.201"
 
-# Sample public DNS for download mode (partial 300+ source implied)
-download_dns=(
-"8.8.8.8"
-"1.1.1.1"
-"9.9.9.9"
-"8.26.56.26"
-"208.67.222.222"
-"1.0.0.1"
-"94.140.14.14"
-"195.46.39.39"
-"185.228.168.9"
-"76.76.2.0"
-"176.103.130.130"
-"45.90.28.0"
-"149.112.112.112"
-"64.6.64.6"
-"77.88.8.8"
-)
-
-# Show list with animation
-show_list() {
-    local -n items=$1
-    for i in "${!items[@]}"; do
-        num=$((i+1))
-        printf "\e[1;${colors[$RANDOM % ${#colors[@]}]}m[%02d] %s\e[0m\n" "$num" "${items[$i]}"
-        sleep 0.02
-    done
-}
-
-# Ping test
-get_ping() {
-    local ip=$1
-    ping -c1 -W1 "$ip" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1 " ms"}'
-}
-
-# Generate 15 DNS
-generate_dns_list() {
-    local -n pool=$1
-    shuffled=($(shuf -e "${pool[@]}" -n 15))
-    for i in "${!shuffled[@]}"; do
-        dns="${shuffled[$i]}"
-        ping=$(get_ping "$dns")
-        ping_display=${ping:-"N/A"}
-        printf "\e[1;36mDNS %02d:\e[0m %s \e[1;33m(Ping: %s)\e[0m\n" "$((i+1))" "$dns" "$ping_display"
-        sleep 0.1
-    done
-}
-
-# Main Menu
-while true; do
-    echo -e "\n$(rand_color 'Select an option:')"
-    echo -e "$(rand_color '[1]') Gaming DNS"
-    echo -e "$(rand_color '[2]') Download DNS"
-    echo -e "$(rand_color '[0]') Exit"
-
-    read -p "$(rand_color 'Enter your choice:') " choice
-
-    case $choice in
-        1)
-            echo -e "\n$(rand_color 'Select a Game:')"
-            show_list games
-            read -p "$(rand_color 'Game number:') " gnum
-            game="${games[$((gnum-1))]}"
-
-            echo -e "\n$(rand_color 'Select Country:')"
-            countries=("${!dns_map[@]}")
-            show_list countries
-            read -p "$(rand_color 'Country number:') " cnum
-            country="${countries[$((cnum-1))]}"
-
-            echo -e "\n$(rand_color "Selected Game: $game")"
-            echo -e "$(rand_color "Selected Country: $country")"
-            IFS=' ' read -ra dns_list <<< "${dns_map[$country]}"
-            generate_dns_list dns_list
-            read -p $'\nPress Enter to return to main menu...'
-            ;;
-        2)
-            echo -e "\n$(rand_color 'Select Country:')"
-            show_list countries
-            read -p "$(rand_color 'Country number:') " cnum
-            generate_dns_list download_dns
-            read -p $'\nPress Enter to return to main menu...'
-            ;;
-        0)
-            echo -e "\e[1;33mGoodbye. Follow our channel on Telegram to see more updates and scripts. @Academi_vpn\e[0m"
-            exit 0
-            ;;
-        *)
-            echo -e "\e[1;31mInvalid option!\e[0m"
-            ;;
-    esac
+for game in "${games[@]}"; do
+  for country in "${countries[@]}"; do
+    key="${game}_${country}"
+    dns_map["$key"]="1.1.1.1 8.8.8.8 4.2.2.4 9.9.9.9 185.51.200.2 178.22.122.100 185.55.226.26 10.202.10.10 178.22.122.2 1.0.0.1 94.140.14.14 94.140.14.15 208.67.222.222 149.112.112.112 208.67.220.220"
+  done
 done
+
+# Select Game
+echo -e "\e[96mSelect your game:\e[0m"
+for i in "${!games[@]}"; do
+  printf "\e[92m[%02d]\e[0m %s\n" "$((i+1))" "${games[$i]}"
+  sleep 0.03
+done
+read -p $'\nEnter game number: ' game_index
+game="${games[$((game_index-1))]}"
+
+# Select Country
+echo -e "\n\e[96mSelect your country:\e[0m"
+for i in "${!countries[@]}"; do
+  printf "\e[95m[%02d]\e[0m %s\n" "$((i+1))" "${countries[$i]}"
+  sleep 0.03
+done
+read -p $'\nEnter country number: ' country_index
+country="${countries[$((country_index-1))]}"
+
+clear
+figlet "DNS Finder" | lolcat
+echo -e "\n\e[93mGame:\e[0m $game"
+echo -e "\e[93mCountry:\e[0m $country"
+echo -e "\e[92mFinding best DNS servers...\e[0m"
+
+key="${game}_${country}"
+dns_list=(${dns_map["$key"]})
+
+if [[ ${#dns_list[@]} -eq 0 ]]; then
+  echo -e "\e[91mNo DNS found for $game in $country.\e[0m"
+  read -p "Press Enter to return..."
+  exit
+fi
+
+# Show 15 DNS with ping
+echo ""
+for i in "${!dns_list[@]}"; do
+  [[ $i -ge 15 ]] && break
+  dns="${dns_list[$i]}"
+  ping_result=$(ping -c 1 -W 1 "$dns" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+  ping_result=${ping_result:-"N/A"}
+  printf "\e[93m[%02d]\e[0m DNS: %-16s \e[96mPing:\e[0m %s ms\n" "$((i+1))" "$dns" "$ping_result"
+  sleep 0.1
+done
+
+# Exit
+echo -e "\n\e[33mGoodbye. Follow our channel on Telegram to see more updates and scripts. @Academi_vpn\e[0m"
