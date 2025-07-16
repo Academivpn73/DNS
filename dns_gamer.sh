70c11ff6757d1f456e1869d2d53f37cc792f8905
Academivpn73 <academi.mah43@gmail.com>
2025-07-16 17:01:18 +0330

    Update dns_gamer.sh

--- dns_gamer.sh
+++ dns_gamer.sh
@@ -1,120 +1,172 @@
 #!/bin/bash
 
-version="1.2.1"
-admin="@MahdiAGM0"
-channel="@Academi_vpn"
+# Auto-install required packages
+for cmd in figlet lolcat; do
+    if ! command -v $cmd &>/dev/null; then
+        echo "Installing missing package: $cmd"
+        apt update -y && apt install -y $cmd
+    fi
+done
 
-# لیست کشورها و بازی‌ها
-countries=("Iran" "Turkey" "UAE" "Saudi Arabia" "Iraq" "Qatar" "Jordan" "Oman" "Kuwait" "Lebanon")
+# Random color function
+colors=(31 32 33 34 36 91 92 93 94 96)
+rand_color() {
+    echo -e "\e[1;${colors[$RANDOM % ${#colors[@]}]}m$1\e[0m"
+}
 
+# Title
+clear
+figlet -c "MAHDI DNS" | lolcat
+echo -e "$(rand_color 'Telegram:') @Academi_vpn"
+echo -e "$(rand_color 'Admin:') @MahdiAGM0"
+echo -e "\e[1;31mVersion: 1.2.0\e[0m\n"
+
+# Game List
 games=(
-"Call of Duty Mobile" "PUBG Mobile" "Free Fire" "Clash Royale" "Mobile Legends" "Fortnite"
-"Valorant" "League of Legends" "CS:GO" "Dota 2" "Apex Legends" "Rainbow Six Siege" "GTA Online"
-"Overwatch 2" "FIFA 24" "Rocket League" "Roblox" "Minecraft" "Warzone" "Genshin Impact"
-"eFootball 2024" "Dead by Daylight" "Battlefield V" "Halo Infinite" "World of Tanks"
-"Paladins" "Naruto Blazing" "Pokemon Unite" "Brawl Stars" "Diablo Immortal" "Black Desert"
-"Elden Ring" "Cyberpunk 2077" "ARK Survival" "NBA 2K24" "Counter Strike 2" "Splatoon 3"
-"Rogue Company" "Warframe"
+"PUBG Mobile"
+"Call of Duty Mobile"
+"Fortnite"
+"Valorant"
+"Free Fire"
+"Clash of Clans"
+"Clash Royale"
+"Apex Legends"
+"League of Legends"
+"Dota 2"
+"CS:GO"
+"Overwatch"
+"Rocket League"
+"FIFA Mobile"
+"Mobile Legends"
+"Brawl Stars"
+"Roblox"
+"Among Us"
+"Warzone"
+"Battlefield"
+"Rainbow Six Siege"
+"World of Tanks"
+"ARK Survival"
+"Paladins"
+"Smite"
+"Genshin Impact"
+"Diablo Immortal"
+"NBA 2K"
+"eFootball"
+"Star Wars Battlefront"
+"Minecraft"
+"Rust"
+"Escape From Tarkov"
+"DayZ"
+"Dead by Daylight"
+"Hearthstone"
+"EVE Online"
+"Pokemon Unite"
+"Naruto Blazing"
+"Injustice 2"
 )
 
-# DNSها برای هر کشور
+# Country DNS map (gaming)
 declare -A dns_map
-dns_map["Iran"]="178.22.122.100 185.51.200.2 10.202.10.10 10.202.10.11 78.157.42.100 185.55.226.26 185.55.225.25 185.124.116.12 185.124.115.11 10.202.10.202 89.165.37.12 10.202.10.5 178.216.248.10 10.202.10.2 195.146.56.1"
-dns_map["Turkey"]="193.140.100.100 195.175.39.39 212.156.4.20 213.74.1.2 213.74.1.1 195.175.39.40 193.140.100.101 193.255.230.10 193.255.230.11 213.74.72.4 213.74.72.5 81.213.132.2 81.213.132.3 193.140.98.2 195.175.170.2"
-dns_map["UAE"]="194.170.1.6 194.170.1.7 217.165.128.6 217.165.129.7 213.42.20.30 213.42.20.20 86.96.100.100 86.96.100.101 86.96.1.1 217.165.129.2 194.170.1.5 194.170.2.2 217.165.131.3 213.42.1.1 217.165.130.130"
-# می‌تونی برای بقیه کشورها هم ادامه بدی...
+dns_map["Iran"]="178.22.122.100 185.51.200.2"
+dns_map["Turkey"]="185.33.33.33 185.38.38.38"
+dns_map["UAE"]="91.74.107.180 91.74.107.181"
+dns_map["Saudi Arabia"]="195.229.241.222 195.229.240.42"
+dns_map["Iraq"]="185.73.112.53 185.73.112.54"
+dns_map["Qatar"]="89.211.30.2 89.211.30.3"
+dns_map["Egypt"]="41.128.225.1 41.128.225.2"
+dns_map["Jordan"]="212.35.65.1 212.35.65.2"
+dns_map["Oman"]="82.178.72.30 82.178.72.31"
+dns_map["Kuwait"]="62.150.97.200 62.150.97.201"
 
-# توابع رنگی
-colors=(31 32 33 34 35 36)
+# Sample public DNS for download mode (partial 300+ source implied)
+download_dns=(
+"8.8.8.8"
+"1.1.1.1"
+"9.9.9.9"
+"8.26.56.26"
+"208.67.222.222"
+"1.0.0.1"
+"94.140.14.14"
+"195.46.39.39"
+"185.228.168.9"
+"76.76.2.0"
+"176.103.130.130"
+"45.90.28.0"
+"149.112.112.112"
+"64.6.64.6"
+"77.88.8.8"
+)
 
-rand_color() {
-  echo -e "\e[1;${colors[$RANDOM % ${#colors[@]}]}m$1\e[0m"
-}
-
-# انیمیشن تایپ
-type_line() {
-  text="$1"
-  delay="${2:-0.005}"
-  for ((i=0; i<${#text}; i++)); do
-    echo -ne "${text:$i:1}"
-    sleep $delay
-  done
-  echo
-}
-
-# نمایش عنوان
-show_title() {
-  clear
-  echo -e "\n\e[1;36m═══════════════════════════════════════════════"
-  type_line "$(rand_color "🚀 Mahdi DNS Panel 🚀")" 0.01
-  echo -e "\e[1;36m═══════════════════════════════════════════════"
-  type_line "$(rand_color "Telegram: $channel")" 0.01
-  type_line "$(rand_color "Admin: $admin")" 0.01
-  type_line "$(rand_color "Version: $version")" 0.01
-  echo -e "\e[1;36m═══════════════════════════════════════════════"
-}
-
-# نمایش لیست با رنگ و شماره و انیمیشن
+# Show list with animation
 show_list() {
-  local -n items=$1
-  for i in "${!items[@]}"; do
-    num=$((i+1))
-    sleep 0.02
-    type_line "$(rand_color "[$num] ${items[$i]}")" 0.001
-  done
+    local -n items=$1
+    for i in "${!items[@]}"; do
+        num=$((i+1))
+        printf "\e[1;${colors[$RANDOM % ${#colors[@]}]}m[%02d] %s\e[0m\n" "$num" "${items[$i]}"
+        sleep 0.02
+    done
 }
 
-# نمایش DNS
-show_dns() {
-  country="$1"
-  echo -e "\n$(rand_color "DNS List for $country:")"
-  IFS=' ' read -ra all_dns <<< "${dns_map[$country]}"
-  i=1
-  for dns in "${all_dns[@]}"; do
-    ping_time=$(ping -c 1 -W 1 $dns 2>/dev/null | grep 'time=' | cut -d'=' -f4 | cut -d' ' -f1)
-    [ -z "$ping_time" ] && ping_time="N/A"
-    echo -e "$(rand_color "[$i]") \e[1;37m$dns\e[0m $(rand_color "(Ping: $ping_time ms)")"
-    i=$((i+1))
-  done
+# Ping test
+get_ping() {
+    local ip=$1
+    ping -c1 -W1 "$ip" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1 " ms"}'
 }
 
-# منو
+# Generate 15 DNS
+generate_dns_list() {
+    local -n pool=$1
+    shuffled=($(shuf -e "${pool[@]}" -n 15))
+    for i in "${!shuffled[@]}"; do
+        dns="${shuffled[$i]}"
+        ping=$(get_ping "$dns")
+        ping_display=${ping:-"N/A"}
+        printf "\e[1;36mDNS %02d:\e[0m %s \e[1;33m(Ping: %s)\e[0m\n" "$((i+1))" "$dns" "$ping_display"
+        sleep 0.1
+    done
+}
+
+# Main Menu
 while true; do
-  show_title
-  echo -e "\n$(rand_color "[1] 🔫 Gaming DNS")"
-  echo -e "$(rand_color "[2] 🚀 Download DNS")"
-  echo -e "$(rand_color "[0] ❌ Exit")"
-  echo
-  read -p "$(rand_color 'Choose an option:')" opt
-  case $opt in
-    1)
-      echo -e "\n$(rand_color "Select Game:")"
-      show_list games
-      read -p "$(rand_color 'Game number:')" gnum
-      selected_game="${games[$((gnum-1))]}"
-      echo -e "\n$(rand_color "Select Country:")"
-      show_list countries
-      read -p "$(rand_color 'Country number:')" cnum
-      selected_country="${countries[$((cnum-1))]}"
-      echo -e "\n$(rand_color "Selected:") $selected_game - $selected_country"
-      show_dns "$selected_country"
-      read -p $'\nPress Enter to return...'
-      ;;
-    2)
-      echo -e "\n$(rand_color "Select Country for Download DNS:")"
-      show_list countries
-      read -p "$(rand_color 'Country number:')" dnum
-      selected_country="${countries[$((dnum-1))]}"
-      show_dns "$selected_country"
-      read -p $'\nPress Enter to return...'
-      ;;
-    0)
-      echo -e "\n$(rand_color 'Goodbye 🌐')"
-      break
-      ;;
-    *)
-      echo -e "$(rand_color 'Invalid choice. Try again.')"
-      sleep 1
-      ;;
-  esac
+    echo -e "\n$(rand_color 'Select an option:')"
+    echo -e "$(rand_color '[1]') Gaming DNS"
+    echo -e "$(rand_color '[2]') Download DNS"
+    echo -e "$(rand_color '[0]') Exit"
+
+    read -p "$(rand_color 'Enter your choice:') " choice
+
+    case $choice in
+        1)
+            echo -e "\n$(rand_color 'Select a Game:')"
+            show_list games
+            read -p "$(rand_color 'Game number:') " gnum
+            game="${games[$((gnum-1))]}"
+
+            echo -e "\n$(rand_color 'Select Country:')"
+            countries=("${!dns_map[@]}")
+            show_list countries
+            read -p "$(rand_color 'Country number:') " cnum
+            country="${countries[$((cnum-1))]}"
+
+            echo -e "\n$(rand_color "Selected Game: $game")"
+            echo -e "$(rand_color "Selected Country: $country")"
+            IFS=' ' read -ra dns_list <<< "${dns_map[$country]}"
+            generate_dns_list dns_list
+            read -p $'\nPress Enter to return to main menu...'
+            ;;
+        2)
+            echo -e "\n$(rand_color 'Select Country:')"
+            show_list countries
+            read -p "$(rand_color 'Country number:') " cnum
+            generate_dns_list download_dns
+            read -p $'\nPress Enter to return to main menu...'
+            ;;
+        0)
+            echo -e "\e[1;33mGoodbye. Follow our channel on Telegram to see more updates and scripts. @Academi_vpn\e[0m"
+            exit 0
+            ;;
+        *)
+            echo -e "\e[1;31mInvalid option!\e[0m"
+            ;;
+    esac
 done
