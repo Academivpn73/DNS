d793bc44cfc2ef633f14898784554b12194f09c2
Academi vpn <academi.mah43@gmail.com>
2025-07-20 22:16:34 +0330

    Update dns_gamer.sh

--- dns_gamer.sh
+++ dns_gamer.sh
@@ -1,141 +1,186 @@
 #!/bin/bash
 
-# Version 2.1 | Telegram: @Academi_vpn | Admin: @MahdiAGM0
+# Version 1.3.0 | Telegram: @Academi_vpn | Admin: @MahdiAGM0
 
 # Colors
-bold="\e[1m"
-reset="\e[0m"
 green="\e[1;32m"
 blue="\e[1;34m"
 cyan="\e[1;36m"
 red="\e[1;31m"
-orange="\e[38;5;208m"
+reset="\e[0m"
+bold="\e[1m"
 
-# Fast Typing
-fast_type() {
+# Typing animation
+type_text() {
     text="$1"
-    speed="${2:-0.00001}"
+    delay="${2:-0.0007}"
     for ((i=0; i<${#text}; i++)); do
         echo -ne "${text:$i:1}"
-        sleep $speed
+        sleep $delay
     done
     echo
 }
 
-# Show Title
+# Random colored title
 show_title() {
     colors=("\e[1;31m" "\e[1;32m" "\e[1;34m" "\e[1;35m" "\e[1;36m")
     rand_color=${colors[$RANDOM % ${#colors[@]}]}
     clear
     echo -e "${rand_color}"
-    fast_type "╔══════════════════════════════════════╗" 0.0001
-    fast_type "║         DNS MANAGEMENT TOOL         ║" 0.0001
-    fast_type "╠══════════════════════════════════════╣" 0.0001
-    fast_type "║  Version: 2.1                       ║" 0.0001
-    fast_type "║  Telegram: @Academi_vpn            ║" 0.0001
-    fast_type "║  Admin: @MahdiAGM0                 ║" 0.0001
-    fast_type "╚══════════════════════════════════════╝" 0.0001
+    type_text "╔══════════════════════════════════════╗" 0.0004
+    type_text "║         DNS MANAGEMENT TOOL         ║" 0.0004
+    type_text "╠══════════════════════════════════════╣" 0.0004
+    type_text "║  Version: 1.3.0                      ║" 0.0004
+    type_text "║  Telegram: @Academi_vpn             ║" 0.0004
+    type_text "║  Admin: @MahdiAGM0                  ║" 0.0004
+    type_text "╚══════════════════════════════════════╝" 0.0004
     echo -e "${reset}"
 }
 
-# DNS List with Multiple DNS Options (with many DNS options for each game)
-declare -A game_dns_map
-game_dns_map=(
-    ["Arena Breakout"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Call of Duty Mobile"]="1.1.1.1 | 8.8.8.8 | 1.0.0.1 | 9.9.9.9"
-    ["PUBG Mobile"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Free Fire"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Fortnite"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Minecraft"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["League of Legends"]="1.1.1.1 | 8.8.8.8 | 9.9.9.9 | 1.0.0.1"
-    ["Valorant"]="1.1.1.1 | 8.8.8.8 | 9.9.9.9 | 1.0.0.1"
-    ["CS:GO"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Dota 2"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Roblox"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Overwatch"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Apex Legends"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Battlefield V"]="1.1.1.1 | 8.8.8.8 | 9.9.9.9 | 1.0.0.1"
-    ["Genshin Impact"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Clash of Clans"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Clash Royale"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["FIFA Online"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Mobile Legends"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Warzone"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Honkai Star Rail"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Among Us"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Rocket League"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Team Fortress 2"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Destiny 2"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Diablo Immortal"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Fall Guys"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Halo Infinite"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Paladins"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Crossfire"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Arena of Valor"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Smite"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Realm Royale"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["World of Tanks"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["World of Warships"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Black Desert"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Lost Ark"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Rust"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["ARK"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Sea of Thieves"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["New World"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Albion Online"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Tarkov"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Brawl Stars"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["War Thunder"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Dead by Daylight"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Naruto Slugfest"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Standoff 2"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
-    ["Bullet Echo"]="185.51.200.2 | 178.22.122.100 | 1.1.1.1 | 8.8.8.8"
+# Games
+pc_games=(
+  "Call of Duty" "PUBG" "Fortnite" "Valorant" "League of Legends"
+  "Dota 2" "CS:GO" "Overwatch" "Rainbow Six Siege" "Apex Legends"
+  "Rocket League" "Minecraft" "Genshin Impact" "Battlefield V" "Roblox"
+  "FIFA 24" "Warzone" "Escape from Tarkov" "War Thunder" "Destiny 2"
+  "Smite" "Halo Infinite" "Fall Guys" "Paladins" "World of Warcraft"
+  "Elden Ring" "Cyberpunk 2077" "ARK" "Sea of Thieves" "Diablo IV"
+  "Arena Breakout"
 )
 
-# Random DNS Function
-random_dns() {
-    dns_list=(${game_dns_map["$selected_game"]})
-    random_index=$((RANDOM % ${#dns_list[@]}))
-    selected_dns=${dns_list[$random_index]}
+console_games=(
+  "Fortnite (Console)" "Warzone (Console)" "FIFA 24 (Console)" "Rocket League (Console)"
+)
+
+countries=("Iran" "Turkey" "UAE" "Saudi Arabia" "Qatar" "Iraq" "Jordan")
+
+# DNS Pools
+dns_pool_game=(
+  "10.202.10.10 10.202.10.11"
+  "78.157.42.101 78.157.42.100"
+  "185.51.200.2 178.22.122.100"
+  "185.55.225.25 185.55.226.26"
+  "9.9.9.9 149.112.112.112"
+  "64.6.64.6 64.6.65.6"
+  "156.154.70.2 156.154.71.2"
+  "159.250.35.250 159.250.35.251"
+  "208.67.222.222 208.67.220.220"
+  "1.1.1.1 1.0.0.1"
+  "8.8.8.8 8.8.4.4"
+)
+
+dns_pool_download=(
+  "1.1.1.1 1.0.0.1"
+  "8.8.8.8 8.8.4.4"
+  "64.6.64.6 64.6.65.6"
+  "156.154.70.2 156.154.71.2"
+  "159.250.35.250 159.250.35.251"
+  "208.67.222.222 208.67.220.220"
+  "185.51.200.2 178.22.122.100"
+  "9.9.9.9 149.112.112.112"
+  "78.157.42.101 78.157.42.100"
+  "185.55.225.25 185.55.226.26"
+  "91.239.100.100 89.223.43.71"
+  "185.228.168.9 185.228.169.9"  # Anti-Censorship
+  "94.140.14.14 94.140.15.15"    # AdGuard
+)
+
+# Ping Test Function
+check_ping() {
+    ip="$1"
+    result=$(ping -c 1 -W 1 "$ip" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+    if [ -z "$result" ]; then
+        echo "Timeout"
+    else
+        echo "${result} ms"
+    fi
 }
 
-# Ping DNS Function
-ping_dns() {
-    echo -n "Pinging DNS: $selected_dns... "
-    ping -c 1 "$selected_dns" > /dev/null && echo "$selected_dns: OK" || echo "$selected_dns: FAILED"
+# Game DNS Menu (PC/Mobile)
+gaming_dns_menu() {
+  clear
+  echo -e "${bold}${green}Select a Game (PC/Mobile):${reset}"
+  for i in "${!pc_games[@]}"; do
+    printf "${blue}[%2d]${reset} %s\n" $((i+1)) "${pc_games[$i]}"
+  done
+  echo -e "${blue}[0]${reset} Back"
+  echo -ne "\n${green}Choose a game: ${reset}"; read gopt
+  [[ "$gopt" == "0" ]] && return
+  [[ -z "${pc_games[$((gopt-1))]}" ]] && echo "Invalid!" && sleep 1 && return
+
+  pick=${dns_pool_game[$RANDOM % ${#dns_pool_game[@]}]}
+  dns1=$(echo "$pick" | awk '{print $1}')
+  dns2=$(echo "$pick" | awk '{print $2}')
+
+  echo -e "\n${cyan}Primary DNS:${reset} $dns1"
+  echo -e "${cyan}Secondary DNS:${reset} $dns2"
+  echo -e "${blue}Ping 1:${reset} $(check_ping $dns1)"
+  echo -e "${blue}Ping 2:${reset} $(check_ping $dns2)"
+  echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Game DNS Menu (for Mobile & PC Games)
-game_dns_menu() {
-    echo -e "\n${bold}${blue}Mobile & PC Games:${reset}"
-    i=1
-    for game in "${!game_dns_map[@]}"; do
-        label="$game"
-        [[ "$game" == "Arena Breakout" ]] && label="${orange}$game (New)${reset}"
-        echo "[$i] $label"
-        ((i++))
-    done
-    echo -n "Choose a game: "; read gidx
-    selected_game=$(printf "%s\n" "${!game_dns_map[@]}" | sed -n "${gidx}p")
-    random_dns
-    echo "DNS for $selected_game: $selected_dns"
-    ping_dns
+# Console DNS Menu
+console_dns_menu() {
+  clear
+  echo -e "${bold}${green}Select a Console Game:${reset}"
+  for i in "${!console_games[@]}"; do
+    printf "${blue}[%2d]${reset} %s\n" $((i+1)) "${console_games[$i]}"
+  done
+  echo -e "${blue}[0]${reset} Back"
+  echo -ne "\n${green}Choose a game: ${reset}"; read gopt
+  [[ "$gopt" == "0" ]] && return
+  [[ -z "${console_games[$((gopt-1))]}" ]] && echo "Invalid!" && sleep 1 && return
+
+  pick=${dns_pool_game[$RANDOM % ${#dns_pool_game[@]}]}
+  dns1=$(echo "$pick" | awk '{print $1}')
+  dns2=$(echo "$pick" | awk '{print $2}')
+
+  echo -e "\n${cyan}Primary DNS:${reset} $dns1"
+  echo -e "${cyan}Secondary DNS:${reset} $dns2"
+  echo -e "${blue}Ping 1:${reset} $(check_ping $dns1)"
+  echo -e "${blue}Ping 2:${reset} $(check_ping $dns2)"
+  echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Main Menu Function
+# Download / Anti-Censorship DNS
+bypass_dns_menu() {
+  clear
+  echo -e "${bold}${green}Bypass / Download DNS:${reset}"
+  for i in "${!countries[@]}"; do
+    printf "${blue}[%2d]${reset} %s\n" $((i+1)) "${countries[$i]}"
+  done
+  echo -e "${blue}[0]${reset} Back"
+  echo -ne "\n${green}Choose region: ${reset}"; read copt
+  [[ "$copt" == "0" ]] && return
+
+  pick=${dns_pool_download[$RANDOM % ${#dns_pool_download[@]}]}
+  dns1=$(echo "$pick" | awk '{print $1}')
+  dns2=$(echo "$pick" | awk '{print $2}')
+
+  echo -e "\n${cyan}Primary DNS:${reset} $dns1"
+  echo -e "${cyan}Secondary DNS:${reset} $dns2"
+  echo -e "${blue}Ping 1:${reset} $(check_ping $dns1)"
+  echo -e "${blue}Ping 2:${reset} $(check_ping $dns2)"
+  echo -e "\n${green}Press Enter to return...${reset}"; read
+}
+
+# Main Menu
 main_menu() {
-    echo -e "${bold}${blue}Main Menu:${reset}"
-    echo "1. Gaming DNS (Mobile & PC)"
-    echo "0. Exit"
-    echo -n "Choose an option: "
-    read choice
-    case $choice in
-        1) game_dns_menu ;;
-        0) echo "Goodbye!"; exit ;;
-        *) echo "Invalid option"; main_menu ;;
+  while true; do
+    show_title
+    echo -e "${blue}[1]${reset} Gaming DNS (PC / Mobile) 🎮"
+    echo -e "${blue}[2]${reset} Console DNS 🕹️"
+    echo -e "${blue}[3]${reset} Download / Anti-Censorship DNS ⬇️"
+    echo -e "${blue}[0]${reset} Exit ❌"
+    echo -ne "\n${green}Choose an option: ${reset}"; read opt
+    case $opt in
+      1) gaming_dns_menu ;;
+      2) console_dns_menu ;;
+      3) bypass_dns_menu ;;
+      0) echo -e "${green}Goodbye 🙏🏻${reset}"; exit ;;
+      *) echo -e "${red}Invalid input!${reset}"; sleep 1 ;;
     esac
+  done
 }
 
-# Show Title and Start Main Menu
-show_title
 main_menu
