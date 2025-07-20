205af7c28e3ce0cf7308155f0b2e2b627c6e322b
Academi vpn <academi.mah43@gmail.com>
2025-07-20 15:28:43 +0330

    Update dns_gamer.sh

--- dns_gamer.sh
+++ dns_gamer.sh
@@ -8,13 +8,14 @@ blue="\e[1;34m"
 cyan="\e[1;36m"
 red="\e[1;31m"
 orange="\e[38;5;214m"
+gray="\e[1;30m"
 reset="\e[0m"
 bold="\e[1m"
 
 # Typing animation
 type_text() {
     text="$1"
-    delay="${2:-0.0003}"  # سرعت تایپ کمی سریع‌تر شد
+    delay="${2:-0.0003}"  # animation speed a bit faster
     for ((i=0; i<${#text}; i++)); do
         echo -ne "${text:$i:1}"
         sleep $delay
@@ -28,17 +29,17 @@ show_title() {
     rand_color=${colors[$RANDOM % ${#colors[@]}]}
     clear
     echo -e "${rand_color}"
-    type_text "╔══════════════════════════════════════╗" 0.0003
-    type_text "║         DNS MANAGEMENT TOOL         ║" 0.0003
-    type_text "╠══════════════════════════════════════╣" 0.0003
-    type_text "║  Version: 1.2.3                      ║" 0.0003
-    type_text "║  Telegram: @Academi_vpn             ║" 0.0003
-    type_text "║  Admin: @MahdiAGM0                  ║" 0.0003
-    type_text "╚══════════════════════════════════════╝" 0.0003
+    type_text "╔══════════════════════════════════════╗" 0.0002
+    type_text "║         DNS MANAGEMENT TOOL         ║" 0.0002
+    type_text "╠══════════════════════════════════════╣" 0.0002
+    type_text "║  Version: 1.2.3                      ║" 0.0002
+    type_text "║  Telegram: @Academi_vpn             ║" 0.0002
+    type_text "║  Admin: @MahdiAGM0                  ║" 0.0002
+    type_text "╚══════════════════════════════════════╝" 0.0002
     echo -e "${reset}"
 }
 
-# Games (original + 10 new games including Arena Breakout)
+# Games (with 10 new, new games names in gray + "NEW" in orange)
 games=(
   "Call of Duty" "PUBG" "Fortnite" "Valorant" "League of Legends"
   "Dota 2" "CS:GO" "Overwatch" "Rainbow Six Siege" "Apex Legends"
@@ -46,14 +47,20 @@ games=(
   "FIFA 24" "Warzone" "Escape from Tarkov" "War Thunder" "Destiny 2"
   "Smite" "Halo Infinite" "Fall Guys" "Paladins" "World of Warcraft"
   "Elden Ring" "Cyberpunk 2077" "ARK" "Sea of Thieves" "Diablo IV"
-  # New games
-  "Arena Breakout" "New Game 1" "New Game 2" "New Game 3" "New Game 4"
-  "New Game 5" "New Game 6" "New Game 7" "New Game 8" "New Game 9" "New Game 10"
+  "\e[90mArena Breakout\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 1\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 2\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 3\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 4\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 5\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 6\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 7\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 8\e[0m \e[38;5;214mNEW\e[0m"
+  "\e[90mNew Game 9\e[0m \e[38;5;214mNEW\e[0m"
 )
 
 countries=("Iran" "Turkey" "UAE" "Saudi Arabia" "Qatar" "Iraq" "Jordan")
 
-# DNS Pools
 dns_pool_game=(
   "10.202.10.10 10.202.10.11"
   "78.157.42.101 78.157.42.100"
@@ -79,14 +86,16 @@ dns_pool_download=(
   "9.9.9.9 149.112.112.112"
   "78.157.42.101 78.157.42.100"
   "185.55.225.25 185.55.226.26"
-  "37.220.84.124 208.67.222.222"  # Added VPN-friendly DNS (OpenNIC)
+  "37.220.84.124 208.67.222.222"
   "74.82.42.42 0.0.0.0"
   "91.239.100.100 89.223.43.71"
   "208.67.220.200 208.67.222.222"
   "10.202.10.10 10.202.10.11"
+  # Add VPN DNS here to help bypass restrictions (example)
+  "208.67.222.222 208.67.220.220"
+  "1.1.1.1 9.9.9.9"
 )
 
-# Ping Test Function
 check_ping() {
     ip="$1"
     result=$(ping -c 1 -W 1 "$ip" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
@@ -97,17 +106,11 @@ check_ping() {
     fi
 }
 
-# Gaming DNS Menu
 gaming_dns_menu() {
   clear
   echo -e "${bold}${green}Select a Game:${reset}"
   for i in "${!games[@]}"; do
-    if (( i >= 30 )); then
-      # رنگ عدد قرمز برای بازی‌های جدید
-      printf "${red}[%2d]${reset} %s ${orange}[NEW]${reset}\n" $((i+1)) "${games[$i]}"
-    else
-      printf "${blue}[%2d]${reset} %s\n" $((i+1)) "${games[$i]}"
-    fi
+    printf "${blue}[%2d]${reset} %b\n" $((i+1)) "${games[$i]}"
   done
   echo -e "${blue}[0]${reset} Back"
   echo -ne "\n${green}Choose a game: ${reset}"; read gopt
@@ -134,7 +137,6 @@ gaming_dns_menu() {
   echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Download DNS Menu
 download_dns_menu() {
   clear
   echo -e "${green}Select country or global DNS:${reset}"
@@ -156,51 +158,59 @@ download_dns_menu() {
   echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Ping Your DNS Menu
-ping_your_dns_menu() {
+ping_dns_menu() {
   clear
   echo -ne "${green}Enter DNS IP to ping: ${reset}"; read user_dns
-  if [[ ! $user_dns =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
-    echo -e "${red}Invalid IP format!${reset}"
-    sleep 2
+  if [[ -z "$user_dns" ]]; then
+    echo -e "${red}No DNS entered!${reset}"
+    sleep 1
     return
   fi
-  echo -e "\n${cyan}DNS:${reset} $user_dns"
-  echo -e "${blue}Ping:${reset} $(check_ping $user_dns)"
+  echo -e "${blue}Pinging DNS $user_dns ...${reset}"
+  ping_result=$(ping -c 1 -W 1 "$user_dns" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+  if [[ -z "$ping_result" ]]; then
+    echo -e "${red}Ping Timeout or DNS unreachable.${reset}"
+  else
+    echo -e "${cyan}DNS:${reset} $user_dns"
+    echo -e "${cyan}Ping:${reset} $ping_result ms"
+  fi
   echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Search Game DNS Menu
 search_game_dns_menu() {
   clear
-  echo -ne "${green}Enter game name to search: ${reset}"; read search_term
-  # Case insensitive search
-  found_indexes=()
+  echo -ne "${green}Enter game name to search DNS: ${reset}"; read game_search
+  if [[ -z "$game_search" ]]; then
+    echo -e "${red}No input entered!${reset}"
+    sleep 1
+    return
+  fi
+
+  # Find matching games
+  matches=()
   for i in "${!games[@]}"; do
-    if [[ "${games[$i],,}" == *"${search_term,,}"* ]]; then
-      found_indexes+=($i)
+    game_name=$(echo -e "${games[$i]}" | sed 's/\x1b\[[0-9;]*m//g')  # Remove color codes
+    if [[ "${game_name,,}" == *"${game_search,,}"* ]]; then
+      matches+=("$i")
     fi
   done
 
-  if [ ${#found_indexes[@]} -eq 0 ]; then
-    echo -e "${red}No games found!${reset}"
+  if [ ${#matches[@]} -eq 0 ]; then
+    echo -e "${red}No games found matching \"$game_search\".${reset}"
     sleep 2
     return
   fi
 
-  clear
-  echo -e "${green}Search results:${reset}"
-  for i in "${found_indexes[@]}"; do
-    if (( i >= 30 )); then
-      printf "${red}[%2d]${reset} %s ${orange}[NEW]${reset}\n" $((i+1)) "${games[$i]}"
-    else
-      printf "${blue}[%2d]${reset} %s\n" $((i+1)) "${games[$i]}"
-    fi
+  # Show matched games
+  echo -e "${green}Found games:${reset}"
+  for idx in "${matches[@]}"; do
+    printf "${blue}[%2d]${reset} %b\n" $((idx+1)) "${games[$idx]}"
   done
-
-  echo -ne "\n${green}Choose a game: ${reset}"; read gopt
-  [[ "$gopt" == "0" ]] && return
-  [[ -z "${games[$((gopt-1))]}" ]] && echo "Invalid!" && sleep 1 && return
+  echo -ne "\n${green}Choose game number: ${reset}"; read gopt
+  if [[ "$gopt" == "0" || -z "${games[$((gopt-1))]}" ]]; then
+    echo "Back"
+    return
+  fi
 
   pick=${dns_pool_game[$RANDOM % ${#dns_pool_game[@]}]}
   dns1=$(echo "$pick" | awk '{print $1}')
@@ -213,25 +223,51 @@ search_game_dns_menu() {
   echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Best DNS (Lowest Ping) Menu
 best_dns_menu() {
   clear
-  echo -e "${green}Checking best DNS with lowest ping... Please wait.${reset}"
+  echo -e "${green}Checking best DNS with lowest average ping... Please wait.${reset}"
+
   best_ping=1000000
   best_dns1=""
   best_dns2=""
 
-  for entry in "${dns_pool_game[@]}" "${dns_pool_download[@]}"; do
+  # Combine all DNS pools
+  all_dns_pools=("${dns_pool_game[@]}" "${dns_pool_download[@]}")
+
+  for entry in "${all_dns_pools[@]}"; do
     dns1=$(echo "$entry" | awk '{print $1}')
     dns2=$(echo "$entry" | awk '{print $2}')
 
-    ping1=$(ping -c 1 -W 1 "$dns1" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
-    ping2=$(ping -c 1 -W 1 "$dns2" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+    # Ping each DNS 3 times and calculate average for dns1
+    ping_sum1=0
+    count1=0
+    for i in {1..3}; do
+      p=$(ping -c 1 -W 1 "$dns1" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+      if [[ $p =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        ping_sum1=$(echo "$ping_sum1 + $p" | bc)
+        ((count1++))
+      fi
+      sleep 0.2
+    done
 
-    # Check if pings are numbers
-    if [[ $ping1 =~ ^[0-9]+(\.[0-9]+)?$ ]] && [[ $ping2 =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-      avg_ping=$(echo "scale=2; ($ping1 + $ping2)/2" | bc)
+    # Ping each DNS 3 times and calculate average for dns2
+    ping_sum2=0
+    count2=0
+    for i in {1..3}; do
+      p=$(ping -c 1 -W 1 "$dns2" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | cut -d' ' -f1)
+      if [[ $p =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        ping_sum2=$(echo "$ping_sum2 + $p" | bc)
+        ((count2++))
+      fi
+      sleep 0.2
+    done
+
+    if (( count1 > 0 && count2 > 0 )); then
+      avg1=$(echo "scale=2; $ping_sum1 / $count1" | bc)
+      avg2=$(echo "scale=2; $ping_sum2 / $count2" | bc)
+      avg_ping=$(echo "scale=2; ($avg1 + $avg2) / 2" | bc)
       avg_ping_int=${avg_ping%.*}
+
       if (( avg_ping_int < best_ping )); then
         best_ping=$avg_ping_int
         best_dns1=$dns1
@@ -250,25 +286,25 @@ best_dns_menu() {
   echo -e "\n${green}Press Enter to return...${reset}"; read
 }
 
-# Main Menu
 main_menu() {
   while true; do
     show_title
     echo -e "${blue}[1]${reset} Gaming DNS 🎮"
     echo -e "${blue}[2]${reset} Download DNS ⬇️"
-    echo -e "${blue}[3]${reset} Best DNS (Lowest Ping) ⭐ ${red}NEW${reset}"
-    echo -e "${blue}[4]${reset} Ping Your DNS 📡 ${red}NEW${reset}"
-    echo -e "${blue}[5]${reset} Search Game DNS 🔍 ${red}NEW${reset}"
-    echo -e "${blue}[0]${reset} Exit ❌"
-    echo -ne "\n${green}Choose an option: ${reset}"; read opt
+    echo -e "${red}[3]${reset} Best DNS (LOWEST PING) \e[31mNEW\e[0m"
+    echo -e "${red}[4]${reset} Ping DNS \e[31mNEW\e[0m"
+    echo -e "${red}[5]${reset} Search Game DNS \e[31mNEW\e[0m"
+    echo -e "${blue}[0]${reset} Exit"
+    echo -ne "\n${green}Choose an option: ${reset}"
+    read opt
     case $opt in
       1) gaming_dns_menu ;;
       2) download_dns_menu ;;
       3) best_dns_menu ;;
-      4) ping_your_dns_menu ;;
+      4) ping_dns_menu ;;
       5) search_game_dns_menu ;;
-      0) echo -e "${green}Goodbye 🙏🏻${reset}"; exit ;;
-      *) echo -e "${red}Invalid input!${reset}"; sleep 1 ;;
+      0) echo -e "${green}Bye!${reset}" ; exit 0 ;;
+      *) echo -e "${red}Invalid option!${reset}" ; sleep 1 ;;
     esac
   done
 }
